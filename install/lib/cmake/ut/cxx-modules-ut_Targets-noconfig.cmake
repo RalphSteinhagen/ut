e8d5269f76d39148aa6748ef5b9ce57b6224df44
Justen Di Ruscio <justend29@gmail.com>
2026-09-15 10:06:22 -0300

    Support CMake installing Boost::ut_module

---
+++ install/lib/cmake/ut/cxx-modules-ut_Targets-noconfig.cmake
@@ -0,0 +1,1 @@
+include("${CMAKE_CURRENT_LIST_DIR}/target-ut__module-noconfig.cmake")
---
+++ install/lib/cmake/ut/target-ut__module-noconfig.cmake
@@ -0,0 +1,4 @@
+set_property(TARGET "Boost::ut_module"
+  PROPERTY IMPORTED_CXX_MODULES_NOCONFIG
+    "boost.ut=${_IMPORT_PREFIX}/include/boost/ut.cppm"
+)
